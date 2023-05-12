88213f52f9a5d730ab56840c20d31da82578681f
Vladislav Verkhovtsev <vlad.verkhovtsev@gmail.com>
2023-05-12 18:21:42 +0300

    реализованы арифметические операции

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.print("Введите первое число: ");
+        int numFirst = new Scanner(System.in).nextInt();
+        System.out.print("Введите второе число: ");
+        int numSecond = new Scanner(System.in).nextInt();
+        System.out.println("Сумма чисел равна: " + (numFirst + numSecond));
+        System.out.println("Разность чисел равна: " + (numFirst - numSecond));
+        System.out.println("Произведение чисел равно: " + (numFirst * numSecond));
+        System.out.println("Частное чисел равно: " + ((double)numFirst / (double)numSecond));
     }
 }
