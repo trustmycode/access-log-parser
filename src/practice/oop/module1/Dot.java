da07211e7d171ff556897cadbddc6fa9a76f814b
Vladislav Verkhovtsev <vlad.verkhovtsev@gmail.com>
2023-05-31 10:30:03 +0300

    reformat code

--- src/practice/oop/module1/Dot.java
+++ src/practice/oop/module1/Dot.java
@@ -1,16 +1,17 @@
 package practice.oop.module1;
 
 public class Dot {
-    public int xCoord;
-    public int yCoord;
 
-    public Dot(int xCoord, int yCoord) {
-        this.xCoord = xCoord;
-        this.yCoord = yCoord;
-    }
+  public final int xCoord;
+  public final int yCoord;
 
-    @Override
-    public String toString() {
-        return "{" + xCoord + ";" + yCoord + "}";
-    }
+  public Dot(int xCoord, int yCoord) {
+    this.xCoord = xCoord;
+    this.yCoord = yCoord;
+  }
+
+  @Override
+  public String toString() {
+    return "(" + xCoord + ";" + yCoord + ")";
+  }
 }
