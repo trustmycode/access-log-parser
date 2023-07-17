5f306b79a01644c89424159766e2996942928ebd
trustmycode <vlad.verkhovtsev@gmail.com>
2023-07-17 19:43:27 +0300

    refactor queue with array

--- GlebsExercises/src/main/java/practice/extracurricular/queue/MyQueueOnArray.java
+++ GlebsExercises/src/main/java/practice/extracurricular/queue/MyQueueOnArray.java
@@ -10,6 +10,8 @@ public class MyQueueOnArray {
   private Object[] elementsArray;
   private int capacity = 5;
   private int size = 0;
+  private int firstIndex = 0;
+  private int lastIndex = 0;
 
   public MyQueueOnArray(Class clazz) {
     this.clazz = clazz;
@@ -19,26 +21,35 @@ public class MyQueueOnArray {
   @Override
   public String toString() {
     StringBuilder stringBuilder = new StringBuilder("[");
-    for (int i = 0; i < size; i++) {
-      if (i == size - 1) {
-        stringBuilder.append(elementsArray[i]);
-        break;
-      }
-      stringBuilder.append(elementsArray[i]);
-      stringBuilder.append(", ");
+    for (int i = firstIndex; i < lastIndex - 1; i++) {
+      stringBuilder.append(elementsArray[i]).append(", ");
     }
-    stringBuilder.append("]");
+    stringBuilder.append(elementsArray[lastIndex - 1]).append("]");
     return stringBuilder.toString();
   }
 
   public boolean offer(Object element) {
     if (element != null) {
       if (element.getClass() == clazz) {
-        if (size == elementsArray.length) {
+        if (size == capacity) {
           capacity *= 2;
           elementsArray = Arrays.copyOf(elementsArray, capacity);
+          lastIndex = size;
         }
-        elementsArray[size++] = element;
+        if (lastIndex == capacity && size < capacity) {
+          int j = firstIndex;
+          for (int i = 0; j < lastIndex; i++, j++) {
+            if (i < lastIndex - firstIndex) {
+              elementsArray[i] = elementsArray[j];
+            } else {
+              elementsArray[i] = null;
+            }
+          }
+          lastIndex = lastIndex - firstIndex;
+          firstIndex = 0;
+        }
+        elementsArray[lastIndex++] = element;
+        size++;
         return true;
       } else {
         throw new FormatException(
@@ -51,22 +62,23 @@ public class MyQueueOnArray {
   }
 
   public Object poll() {
-    if (size == 0) {
+    if (isEmpty()) {
       throw new NoSuchElementException("Queue is empty");
     }
-    Object tmp = elementsArray[0];
-    for (int i = 1; i < size; i++) {
-      elementsArray[i-1] = elementsArray[i];
+    Object tmp = elementsArray[firstIndex];
+    if (firstIndex == lastIndex) {
+      firstIndex = 0;
     }
+    elementsArray[firstIndex++] = null;
     size--;
     return tmp;
   }
 
   public Object peek() {
-    if (size == 0) {
+    if (isEmpty()) {
       throw new NoSuchElementException("Queue is empty");
     }
-    return elementsArray[0];
+    return elementsArray[firstIndex];
   }
 
   public int size() {
@@ -80,16 +92,42 @@ public class MyQueueOnArray {
   static class Main {
 
     public static void main(String[] args) {
-      MyQueue queue = new MyQueue(String.class);
+      MyQueueOnArray queue = new MyQueueOnArray(String.class);
       System.out.println(queue.size());
       System.out.println(queue.offer("asd"));
       System.out.println(queue.offer("fasd"));
+      System.out.println(queue.offer("wads"));
+      System.out.println(queue.offer("hytjn"));
+      System.out.println(queue.offer("xczvb"));
       System.out.println(queue);
       System.out.println(queue.size());
       System.out.println(queue.peek());
       System.out.println(queue.poll());
       System.out.println(queue);
       System.out.println(queue.size());
+      System.out.println(queue.peek());
+      System.out.println(queue.poll());
+      System.out.println(queue);
+      System.out.println(queue.size());
+      System.out.println(queue.offer("asd"));
+      System.out.println(queue);
+      System.out.println(queue.offer("asd"));
+      System.out.println(queue);
+      System.out.println(queue.offer("jklfg"));
+      System.out.println(queue.offer("jkasdfg"));
+      System.out.println(queue.offer("jkqweg"));
+      System.out.println(queue);
+      System.out.println(queue.poll());
+      System.out.println(queue.poll());
+      System.out.println(queue.poll());
+      System.out.println(queue.poll());
+      System.out.println(queue.poll());
+      System.out.println(queue);
+      System.out.println(queue.offer("fasd"));
+      System.out.println(queue.offer("wads"));
+      System.out.println(queue.offer("hytjn"));
+      System.out.println(queue.offer("xczvb"));
+      System.out.println(queue);
     }
   }
 }
