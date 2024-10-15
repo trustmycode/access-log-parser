b1cbb47e83d4a3bf816fffe513cb24ef8fba0a36
trustmycode <vlad.verkhovtsev@gmail.com>
2024-10-15 17:27:51 +0300

    Для вебинара

--- src/practice/oop/demo/stream/LambdaExample.java
+++ src/practice/oop/demo/stream/LambdaExample.java
@@ -9,10 +9,18 @@ public class LambdaExample {
         List<Student> students = Arrays.asList(new Student(7, "Alice", new Teacher("Mr. Smith")), new Student(6, "Bob", new Teacher("Mrs. Jones")), new Student(7, "Charlie", new Teacher("Mr. Brown")), new Student(8, "David", new Teacher("Mr. White")));
 
         // Фильтрация студентов по классу = 7
-        List<Student> filteredStudents = students.stream().filter(student -> student.getNumOfClass() == 7)  // Лямбда для фильтрации
+        List<Teacher> teachersOfFilteredStudents = students.stream()
+                .filter(student -> student.getNumOfClass() == 7)  // Лямбда для фильтрации
+                .map(Student::getFirstTeacher)
+                .toList();
+
+        List<Student> filteredStudents = students.stream()
+                .filter(student -> student.getNumOfClass() == 7)  // Лямбда для фильтрации
                 .peek(student -> student.setNameOfStudent(student.getNameOfStudent().toUpperCase()))
                 .toList();
 
+
+        teachersOfFilteredStudents.forEach(System.out::println);
         // Вывод отфильтрованных студентов
         filteredStudents.forEach(System.out::println);
     }
